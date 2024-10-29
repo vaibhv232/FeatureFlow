13c94477fd14466c4df0ba2f1d83bbdd80a5e30b
vaibhv232 <vaibhav.official.232@gmail.com>
2024-10-30 03:37:42 +0530

    Frontend-1

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: 'class', // This allows you to toggle dark mode via a CSS class
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
